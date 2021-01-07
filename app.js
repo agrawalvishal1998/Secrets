d271453d91d1ee4b0ac18c97400ba6b87d146bf4
vishal agrawal <agrawalvishal1997@gmail.com>
2021-01-07 19:54:22 +0530

    Hashing using bycrypt

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+// const encrypt = require('mongoose-encryption');
+// const md5 = require('md5'); used for hashing.We are using bcrypt intead of md5 to make our password safer.
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 
 const app = express();
@@ -18,7 +21,7 @@ const userSchema=new mongoose.Schema({
 
 // SECRET is the encryption key
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });//encryptedFields is used to tell that we only want to encrypt password.
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] }); encryptedFields is used to tell that we only want to encrypt password.
 const User=new mongoose.model("User",userSchema);
 
 app.set('view engine', 'ejs');
@@ -42,17 +45,24 @@ app.get("/register",function(req,res){
 });
 
 app.post("/register",function(req,res){
-     const newUser=new User({
-       email:req.body.username,
-       password:req.body.password
-     });
 
-     newUser.save(function(err){
-       if(err)
-       console.log(err);
-       else
-       res.render("secrets");
-     })
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+
+    const newUser=new User({
+      email:req.body.username,
+      password:hash
+    });
+
+    newUser.save(function(err){
+      if(err)
+      console.log(err);
+      else
+      res.render("secrets");
+    })
+
+   });
+
+
 });
 
 app.post("/login",function(req,res){
@@ -64,16 +74,20 @@ app.post("/login",function(req,res){
     console.log(err)
     else
     {
-      if(foundUser.password===password)
-      res.render("secrets");
+      if(foundUser)
+      {
+        bcrypt.compare(password,foundUser.password, function(err, result) {
+          if(result===true)
+          {
+            res.render("secrets");
+          }
+      });
+
+      }
 
     }
   });
 });
-
-
-
-
 
 
 app.listen(3000, function() {
